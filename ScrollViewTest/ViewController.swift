06a7eb4ff2fe29b76f966d0b29c43cceffc914c5
Mohamed Soussi <mohameddsoussi@gmail.com>
2017-05-26 15:12:01 -0400

    initial commit

---
+++ ScrollViewTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  ScrollViewTest
+//
+//  Created by Mohamed Soussi on 5/26/17.
+//  Copyright © 2017 modsoussi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView.contentSize = CGSize(width: 375, height: 1000)
+        scrollView.isScrollEnabled = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
